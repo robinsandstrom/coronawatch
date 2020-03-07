abf75871ef6acccfe8a9c0a753e127b08fe572cd
robinsan <robinsan@kth.se>
2020-03-07 15:01:17 +0100

    Fixes

--- insight/models.py
+++ insight/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class CoronaCase(models.Model):
+
+    date = models.DateTimeField(blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    infected = models.IntegerField(default=0)
--- insight/views.py
+++ insight/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from insight.backend import load_csv
+from insight.models import CoronaCase
 
 def index(request):
     template = 'insight/home.html'
     data = load_csv()
+    cases = CoronaCase.objects.all().order_by('date')
     return render(request, template, context={
                                             'data': data,
+                                            'cases': cases
                                             })
