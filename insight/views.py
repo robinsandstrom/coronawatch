fe98b50701f22bc5a01d428c268427c6c98ba31f
robinsan <robinsan@kth.se>
2020-03-08 14:00:07 +0100

    hello

--- insight/views.py
+++ insight/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 from insight.backend import load_csv, populate_regional_data
 from insight.models import CoronaCase
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def index(request):
     template = 'insight/home.html'
@@ -19,11 +19,23 @@ def index(request):
     date = datetime.today()
     prognosis = data[8]
 
+    date_from = datetime.now() - timedelta(days=1)
+
+    new_cases = cases.filter(date__gte=date_from)
+
+    total_new = 0
+    for case in new_cases:
+        total_new+=case.infected
+
+    last_updated = cases.last().date
+
     return render(request, template, context={
                                             'data': data,
                                             'cases': cases,
                                             'regional_data': regional_data,
                                             'ordered_regional_data': ordered_regional_data,
                                             'total': total,
-                                            'prognosis': prognosis
+                                            'prognosis': prognosis,
+                                            'new_cases': total_new,
+                                            'last_updated': last_updated
                                             })
