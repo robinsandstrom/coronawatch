0a425e417fed8cc73f68b553b5f1fe4125e8dddb
robinsan <robinsan@kth.se>
2020-03-08 17:23:51 +0100

    fix

--- corona/settings.py
+++ corona/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$8t4_!4*v137lkl-i)avb-vn4%!n7l&m=#vgspg+m64xqqkh!7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*.herokuapp.com, coronawatch.se, 127.0.0.1']
 
